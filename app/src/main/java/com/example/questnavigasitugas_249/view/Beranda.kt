a6247fde33bccbc786f6c982f9a4e51adaf08767
Cruelasss <irula5198@gmail.com>
2025-11-07 18:45:00 +0700

    Memanggil Gambar dari drawable

--- app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
@@ -1,11 +1,22 @@
 package com.example.questnavigasitugas_249.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.questnavigastugas_249.R
+
+
+
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
     onNextButtonClicked: () -> Unit
 ) {
+    Scaffold { isiRuang ->
+        val regar = painterResource(R.Drawable.regar)
+    }
+}
     )
