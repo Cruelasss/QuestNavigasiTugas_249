da65a6cb747bae60fec97c5d4af44d0b3319186f
Cruelasss <irula5198@gmail.com>
2025-11-07 19:06:58 +0700

    Selesaikan tampilan dan fungsi form pendaftaran dalam ElevatedCard

--- app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
@@ -58,6 +58,113 @@ import androidx.compose.ui.unit.sp
                         modifier = Modifier.fillMaxWidth()
                     )
                 }
+                Spacer(modifier = Modifier.height(20.dp))
+
+                ElevatedCard(modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .padding(all = 10.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color.White),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 50.dp)
+                ) {
+                    Text(text = "NAMA LENGKAP",modifier = Modifier.padding(all = 10.dp), fontWeight = FontWeight.Bold)
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        label = { Text(text = "Isian Nama Lengkap")},
+                        modifier = Modifier.padding(10.dp)
+                            .fillMaxWidth(1f),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.DarkGray,
+                            focusedContainerColor = Color.White,
+                            unfocusedContainerColor = Color.White,
+                            focusedBorderColor = Color.Blue,
+                            unfocusedBorderColor = Color.Gray,
+                        )
+                    )
+
+                    Text(text = "JENIS KELAMIN",modifier = Modifier.padding(all = 10.dp), fontWeight = FontWeight.Bold)
+                    Column() {
+                        gender.forEach { item ->
+                            Column(
+                                modifier = Modifier
+                                    .selectable(
+                                        selected = textJK == item,
+                                        onClick = { textJK == item }
+                                    )
+                            ){
+                                Row() {
+                                    RadioButton(
+                                        selected = textJK == item,
+                                        onClick = { textJK = item }
+                                    )
+                                    Text(
+                                        text = item,
+                                        modifier = Modifier.padding(top = 10.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Text(text = "STATUS PERKAWINAN",modifier = Modifier.padding(all = 10.dp), fontWeight = FontWeight.Bold)
+                    Column() {
+                        status1.forEach { item ->
+                            Column(
+                                modifier = Modifier
+                                    .selectable(
+                                        selected = textStatus == item,
+                                        onClick = {textStatus == item}
+                                    )
+                            ){
+                                Row() {
+                                    RadioButton(
+                                        selected = textStatus == item,
+                                        onClick = {textStatus = item}
+                                    )
+                                    Text(
+                                        text = item,
+                                        modifier = Modifier.padding(top = 10.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    Text(text = "ALAMAT LENGKAP",modifier = Modifier.padding(all = 10.dp), fontWeight = FontWeight.Bold)
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        label = { Text(text = "Alamat Lengkap")},
+                        modifier = Modifier.padding(10.dp)
+                            .fillMaxWidth(1f),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.DarkGray,
+                            focusedContainerColor = Color.White,
+                            unfocusedContainerColor = Color.White,
+                            focusedBorderColor = Color.Blue,
+                            unfocusedBorderColor = Color.Gray,
+                        )
+                    )
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth(fraction = 1f)
+                            .padding(all = 10.dp),
+                        onClick = {
+                            jenis = textJK
+                            status = textStatus
+                            onBackToBtnClick()
+                        }
+                    ){
+                        Text(text = stringResource(id = R.string.submit))
+                    }
+                }
+            }
+        }
+}
             }
             ) {
 
