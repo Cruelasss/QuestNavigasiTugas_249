04e6c93d05b22a7eeb46e374254de02e4745523e
Cruelasss <irula5198@gmail.com>
2025-11-07 19:03:41 +0700

    Implementasi Box fillMaxSize sebagai container utama

--- app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
@@ -1,10 +1,14 @@
 package com.example.questnavigasitugas_249.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @OptIn (ExperimentalMaterial3Api::class))
         @Composable
@@ -23,5 +27,13 @@ import androidx.compose.runtime.setValue
 
             val gender: List<String> = listOf("Laki-laki", "Perempuan")
             val status1: List<String> = listOf("Janda", "Lajang", "Duda")
+
+            Box(modifier = Modifier.fillMaxSize()
+                    Column(modifier = Modifier
+                .padding(top = 50.dp)
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            ) {
 
         }
