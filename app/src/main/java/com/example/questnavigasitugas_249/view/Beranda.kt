0e166f0a24c9d8f0adb23f5c96403647eed6e9e1
Cruelasss <irula5198@gmail.com>
2025-11-07 18:41:13 +0700

    Buat composable HomePage awal

--- app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigasitugas_249.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage(
+    onNextButtonClicked: () -> Unit
+) {
+    )
--- app/src/main/java/com/example/questnavigasitugas_249/view/List.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/List.kt
@@ -1,2 +1,4 @@
 package com.example.questnavigasitugas_249.view
 
+
+
