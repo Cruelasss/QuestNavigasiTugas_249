634f6f14579798af8c8c51458c16192696900bf8
Cruelasss <irula5198@gmail.com>
2025-11-07 18:59:47 +0700

    Membuat Composable Form Kedua

--- app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Formulir.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigasitugas_249.view
 
+import androidx.compose.runtime.Composable
+
+@OptIn (ExperimentalMaterial3Api::class))
+        @Composable
+        fun FormDataDiri(
+            onBackToBtnClick: () -> Unit
+        ){
+
+        }
