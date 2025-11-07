11679a04fbdcf8072fa983d6feb791a2917fc75b
Cruelasss <irula5198@gmail.com>
2025-11-07 18:48:21 +0700

    Tambahkan teks selamat datang dengan styling kustom

--- app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_249/view/Beranda.kt
@@ -8,10 +8,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.questnavigastugas_249.R
 
 
@@ -31,7 +37,14 @@ fun HomePage(
                 .padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )
+        ){
+            Text(text= stringResource(id = R.string.welcome), modifier = Modifier,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 40.sp,
+                color = Color(0xFFAD6FBF)
+            )
+        }
     }
 }
     )
